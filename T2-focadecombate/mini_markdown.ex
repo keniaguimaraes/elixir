acb972dbecd69db2cee85da7e1a3aadd6335a7d4
Gustavo Santos <gustavosisa@outlook.com>
2021-11-15 11:59:44 -0300

    feat: mini_markdown.ex

---
+++ T2-focadecombate/mini_markdown.ex
@@ -0,0 +1,27 @@
+defmodule MiniMarkdown do
+  def to_html(text) do
+    text
+      |> italics()
+      |> bold()
+      |> paragraph()
+  end
+
+  def bold(text) do
+    Regex.replace(~r/\*\*(.*)\*\*/,text, "<strong>\\1</strong>")
+  end
+
+  def italics(text) do
+    Regex.replace(~r/\*(.*)\*/,text, "<bold>\\1</bold>")
+  end
+
+  def paragraph(text) do
+    Regex.replace(~r/(\r\n|\r|\n|^)+([^\r\n]+)((\r\n|\r|\n)+$)?/,text, "<p>\\2</p>")
+  end
+
+  def h1(text) do
+    Regex.replace(~r/^# (.*)$/,text, "<h1>\\1</h1>")
+  end
+  def h1(text) do
+    Regex.replace(~r/([#]{2}) (.*)$/,text, "<h2>\\1</h2>")
+  end
+end
